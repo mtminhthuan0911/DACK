2b62a8194aa57f10b71f7b2eb617d18e68979a11
thuanvm <thuanvm@nvpcloud.com>
2021-05-24 00:23:33 +0700

    Upload source  - App - Lan 2

---
+++ app/src/main/java/com/example/yourdoctor/danhmuc.java
@@ -0,0 +1,14 @@
+package com.example.yourdoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class danhmuc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danhmuc);
+    }
+}
