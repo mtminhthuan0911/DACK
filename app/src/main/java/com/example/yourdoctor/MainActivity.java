7bdd3d14d0f392119bea124e3aec087d229f5811
thuanvm <thuanvm@nvpcloud.com>
2021-06-30 20:29:29 +0700

    Update Giao Dien - Trang Chu , Danh Muc

--- app/src/main/java/com/example/yourdoctor/MainActivity.java
+++ app/src/main/java/com/example/yourdoctor/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-
     }
 
 }
